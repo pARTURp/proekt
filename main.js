739f32298d3b73f59ec76d7d411635f53fd38ab1
pARTURp <arturakmatov13@gmail.com>
2024-07-04 15:11:50 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// let balance = 10000
+// document.addEventListener('click', () => {
+//     if (balance >= 1000){
+//     balance -= 1000
+//     console.log("ваш баланс: " + balance);
+//     }   else {
+//         alert("недостаточно средств")
+//     }
+// })
+
+console.log("ваш баланс: " + balance);
